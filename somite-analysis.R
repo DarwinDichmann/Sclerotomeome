291b2da4282c24b325771b2fac5f0283f206a6d4
DarwinDichmann <dichmann@berkeley.edu>
2016-05-07 17:40:20 -0700

    Added code and pdf for Figure 2 scatter plot.

--- somite-analysis.R
+++ somite-analysis.R
@@ -133,11 +133,75 @@ load("DESeq-objects/DDS_somites.rda")
 # Write Scl and peat DEGs to files.
 dir.create("DEG-lists")
 
-
 deg_scl <- writeDESeqResults(dds = dds_som, 
                              num = "Directed", 
                              denom = "Control",
                              return_data = TRUE, 
                              filename = "DEG-lists/deg_scl.txt")
+
+save(deg_scl, file = "DESeq-objects/deg_scl.rda")
+
+
+#### Scatter plot of DE genes. ####
+
+# Extract mean normalized counts from scl data for scatter plot.
+scl_counts <- as.data.frame(counts(dds_som[, 1:8], normalized = TRUE))
+scl_means <- summarise(.data = scl_counts,
+                       meanCTR = rowMeans(scl_counts[, 1:4]), 
+                       meanDIR = rowMeans(scl_counts[, 5:8]))
+row.names(scl_means) <- rownames(scl_counts)
+
+# Make subset with DEG to highlight on plot.
+deg_scl_names <- row.names(deg_scl)
+deg_scl_means <- subset(scl_means, rownames(scl_means) %in% rownames(deg_scl))
+# scl_means[c("Pax1", "Pax9", "AI646519"), ]
+
+# Fit a linear model for plot.
+lm_fit <- lm(meanDIR ~ meanCTR, scl_means)
+
+# Extract means of interesting genes to plot.
+deg_scl_up <- c("Pax1", "Pax9", "AI646519", "Nkx3-1", "Heyl", "Bmp3", "Nbl")
+deg_scl_dn <- c("Id1", "Id2", "Id3", "Myf5", "Pax8")
+up_means <- subset(deg_scl_means, rownames(deg_scl_means) %in% deg_scl_up )
+dn_means <- subset(deg_scl_means, rownames(deg_scl_means) %in% deg_scl_dn)
+
+# Plot.
+de_plot <- ggplot(scl_means, aes(log2(meanCTR), log2(meanDIR))) 
+de_plot <- de_plot + theme_cowplot()
+de_plot <- de_plot + xlab("Control\nlog2(Normalized Counts)")
+de_plot <- de_plot + ylab("Directed\nlog2(Normalized counts)")
+de_plot <- de_plot + geom_point(colour="grey50", size= 2, alpha= 0.3)
+# Add regression line.
+de_plot <- de_plot + geom_abline(data = lm_fit, colour = "grey20")
+de_plot <- de_plot + annotate("text", colour = "grey20", 
+                              label = "slope = 1.095",
+                              x = 14, y = 18, size = 4)
+# Add DE genes
+de_plot <- de_plot + geom_point(data = deg_scl_means, 
+                                colour = "#1f78b4", 
+                                size = 3, 
+                                alpha = 0.7)
+# Add interesting up genes.
+de_plot <- de_plot + geom_point(data = up_means, 
+                                colour = "#e31a1c",
+                                size = 3)
+de_plot <- de_plot + geom_text(data = up_means, 
+                               aes(label = rownames(up_means)), 
+                               colour = "#e31a1c",
+                               size = 4,
+                               hjust = 1, vjust = -1)
+# Add interesting down genes.
+de_plot <- de_plot + geom_point(data = dn_means, 
+                                colour= "#ff7f00", 
+                                size= 3) 
+de_plot <- de_plot + geom_text(data = dn_means, 
+                               aes(label = rownames(dn_means) ), 
+                               colour = "#ff7f00", 
+                               size = 4,
+                               hjust = 0, vjust= 1.5)
+
+ggsave(de_plot, file = "Figure-plots/Fig2.pdf", width = 6, height = 6)
+
+
 
 
